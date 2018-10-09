15a06e1024b2155077a246365de2f09664ae8bd8
Luke Brierley <ldbrierley@gmail.com>
2018-10-09 13:08:09 +0100

    added random number generation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate rand;
+use rand::Rng;
 use std::io;
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret numberis:{}", secret_number);
     println!("please input guess");
     let mut guess = String::new();
     io::stdin()
