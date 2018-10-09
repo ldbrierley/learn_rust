24bdccb671a0e359ca97352b3f6bc6321f188d67
Luke Brierley <ldbrierley@gmail.com>
2018-10-09 14:27:33 +0100

    match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Guess the number!");
@@ -11,4 +12,9 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read line");
     println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("you win"),
+    }
 }
