1be0c291ae4230b366dfe46eaf09c1301682c1c8
Luke Brierley <ldbrierley@gmail.com>
2018-10-25 13:52:00 +0100

    made game with numbers

--- all_numbers/src/main.rs
+++ all_numbers/src/main.rs
@@ -1,5 +1,7 @@
 // Make a prgram that prints out all number starting form 0 to infinity.
 
 fn main() {
-    println!("Hello, world!");
+    for number in 1.. {
+        println!("{}", number)
+    }
 }
