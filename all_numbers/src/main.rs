af7353e3dc163c6d1513ba6983737adfae726e62
Luke Brierley <ldbrierley@gmail.com>
2018-10-25 14:46:07 +0100

    make fizzbuzz

--- all_numbers/src/main.rs
+++ all_numbers/src/main.rs
@@ -1,7 +1,17 @@
 // Make a prgram that prints out all number starting form 0 to infinity.
 
 fn main() {
-    for number in 1.. {
-        println!("{}", number)
+    for number in (1..) {
+        if number % 3 == 0 {
+            if number % 5 == 0 {
+                println!("fizzbuzz")
+            } else {
+                println!("fizz")
+            }
+        } else if number % 5 == 0 {
+            println!("buzz")
+        } else {
+            println!("{}", number);
+        }
     }
 }
